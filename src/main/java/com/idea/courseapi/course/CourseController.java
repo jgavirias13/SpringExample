bbcfb26170fa21ba9ff8a175b914573548504953
Juan Pablo Gaviria <jgavirias13@gmail.com>
2020-05-05 22:04:29 -0500

    Cursos
    Se termina el endpoint de cursos

--- src/main/java/com/idea/courseapi/course/CourseController.java
+++ src/main/java/com/idea/courseapi/course/CourseController.java
@@ -2,13 +2,11 @@ package com.idea.courseapi.course;
 
 import com.idea.courseapi.topic.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class CourseController {
 
     @Autowired
--- src/main/java/com/idea/courseapi/course/CourseService.java
+++ src/main/java/com/idea/courseapi/course/CourseService.java
@@ -1,10 +1,12 @@
 package com.idea.courseapi.course;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CourseService {
 
     @Autowired
